4e5e9d1913b571c597a39d9df335e2772b167a54
henrysmith-cell <289720762+henrysmith-cell@users.noreply.github.com>
2026-07-09 22:45:04 +0700

    Add files via upload

---
+++ 67_Chon.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[100];
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    // Selection Sort
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[min])
+                min = j;
+        }
+
+        swap(a[i], a[min]);
+    }
+
+    cout << "Mang sau khi sap xep:\n";
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+
+    return 0;
+}
